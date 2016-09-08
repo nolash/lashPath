0c8cf69beeef69bc860aee55a65f86803dcb3a3d
Louis Holbrook <accounts@holbrook.no>
2016-09-08 18:40:43 +0200

    lash_mapSimpleFree() failed, attempt to double free layers. FIXED! lash_mapSimpleClip() clipped outside max y boundary of source map. FIXED!

--- include/liblashgame/lash_game_map.h
+++ include/liblashgame/lash_game_map.h
@@ -5,6 +5,35 @@
 
 #include "liblashgame/lash_game_standard.h"
 
+	// considered doing this with pointer to pointer, to allow one map object be a clip of another and point to the same data sources, but:
+	//
+	// upside: data only in one place; change it in map, change it in clip
+	// downsize: pointers are twice the size of unsigned int, twice the memory to reserve for maps and then later move for clips
+	// Here is a possible implementation:
+	//
+	// keep the actual values in a separate array,
+	// (this is the same solution as in lash_game_path_simple)
+	//
+	// c.layervalues = (unsigned int*)malloc(sizeof(unsigned int) * 400);
+	// *c.layervalues = 10;
+	// *c.layervalues = 20;
+	//
+	// Highest pointer level is pointers to the individual layers
+	//
+	// c.layers = (unsigned int ***)malloc(sizeof(unsigned int) * 2);
+	//
+	// Second pointer level the individual entries are pointers to the values in c.layervalues
+	//
+	// *(c.layers + 0) = (unsigned int **)malloc(sizeof(unsigned int) * 100);
+	// *(c.layers + 1) = (unsigned int **)malloc(sizeof(unsigned int) * 100);
+	//
+	// Pointers can be assigned on the third level
+	//
+	// **(c.layers + 0) = c.layervalues;
+	// *(*(c.layers + 1) + 1) = c.layervalues;
+	//
+	// printf("pointer address layer %p : %p layer %p : %p in layervalues %p\n", *(c.layers + 0), **(c.layers + 0), *(c.layers + 1), *(*(c.layers + 1) + 1), c.layervalues); 
+	
 typedef unsigned short lash_map_simple_layer_item_t;
 
 typedef struct lash_map_simple_layer_t lash_map_simple_layer_t;
@@ -23,10 +52,29 @@ struct lash_map_simple_t {
 	lash_map_simple_layer_t *layer_path;
 };
 
+enum lash_game_map_alignment {
+	LASH_GAME_MAP_OFFSET_CENTER,
+	LASH_GAME_MAP_OFFSET_TOPLEFT,
+	LASH_GAME_MAP_OFFSET_BOTTOMLEFT,
+	LASH_GAME_MAP_OFFSET_BOTTOMRIGHT,
+	LASH_GAME_MAP_OFFSETP_TOPRIGHT
+};
+
 lash_map_simple_t * lash_mapSimpleInit(lash_map_simple_t *map, unsigned int *w, unsigned int *h);
 void lash_mapSimpleFree(lash_map_simple_t *map);
 lash_map_simple_layer_t * lash_mapSimpleAddLayer(lash_map_simple_t *map);
 void lash_mapSimpleLayerPoke(lash_map_simple_layer_t *layer, const lash_game_map_index_t index, const lash_map_simple_layer_item_t value);
 lash_map_simple_layer_item_t lash_mapSimpleLayerPeek(lash_map_simple_layer_t *layer, const lash_game_map_index_t index);
+lash_map_simple_t * lash_mapSimpleClip(lash_map_simple_t *clipmap, lash_map_simple_t *sourcemap, const unsigned int clip_x_radius, const unsigned int clip_y_radius, const lash_game_map_index_t clip_origin_index, enum lash_game_map_alignment clip_origin_alignment, int include_path_only);
 
 #endif // LASH_GAME_MAP_SIMPLE_H_
+
+
+
+/**
+ * \fn  lash_mapSimpleClip(lash_map_simple_t *clipmap, lash_map_simple_t *sourcemap, const unsigned int clip_x_radius, const unsigned int clip_y_radius, const lash_game_map_index_t clip_origin_index, enum lash_game_map_clip_alignment clip_origin_alignment, int include_path_only)
+ * 
+ * \todo Support for defining other alignments from index (especially topleft). Currently it only calculates clip with index in center and in "radius" rectangle around.
+ * \todo Support for copying all layers, now it only copies the first (layer_path)
+ * \todo Support for copying (back) from clip to map
+*/
--- src/liblashgame/lash_game_map.c
+++ src/liblashgame/lash_game_map.c
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 #include "liblashgame/lash_game_map.h"
 #include "liblashgame/lash_game_standard.h"
 
@@ -14,15 +17,24 @@ lash_map_simple_t * lash_mapSimpleInit(lash_map_simple_t *map, unsigned int *w, 
 }
 
 void lash_mapSimpleFree(lash_map_simple_t *map) {
-	free(map->w);
-	free(map->h);
-	free(map->layers);
-	free(map->layer_path);
+	int i;
+	
+	if (map->w != NULL)
+		free(map->w);
+	
+	if (map->h != NULL)
+		free(map->h);
+	
+	for (i = 0; i < map->layer_count; i++) {
+		if ((map->layers + i) != NULL)
+			free(map->layers + i);
+	}
+	
 }
 
 lash_map_simple_layer_t * lash_mapSimpleAddLayer(lash_map_simple_t *map) {
 	lash_map_simple_layer_t *newlayer;
-	newlayer = (lash_map_simple_layer_t*)calloc(*(map->w) * *(map->h), sizeof(lash_map_simple_layer_t));
+	newlayer = (lash_map_simple_layer_t*)malloc(*(map->w) * *(map->h) * sizeof(lash_map_simple_layer_item_t));
 	if (newlayer == NULL)
 		return NULL;
 	*(map->layers + map->layer_count) = newlayer;
@@ -42,4 +54,70 @@ lash_map_simple_layer_item_t lash_mapSimpleLayerPeek(lash_map_simple_layer_t *la
 		return -1;
 	
 	return (layer+index)->val;
+}
+
+lash_map_simple_t * lash_mapSimpleClip(lash_map_simple_t *clipmap, lash_map_simple_t *sourcemap, const unsigned int clip_x_radius, const unsigned int clip_y_radius, const lash_game_map_index_t clip_origin_index, enum lash_game_map_alignment clip_origin_alignment, int include_path_only) {
+
+	int i;
+	
+	unsigned int delta_left;
+	unsigned int delta_right;
+	unsigned int delta_top;
+	unsigned int delta_bottom;
+	
+	unsigned int clip_y_top;
+	unsigned int clip_y_bottom;
+	unsigned int clip_x_left;
+	unsigned int clip_x_right;
+	
+	unsigned int clip_width;
+	unsigned int clip_height;
+	
+	//lash_game_coords_cartesian_t map_coords_bounds;
+	lash_game_map_index_t map_index_last;
+	
+	// if no valid source map
+	if (sourcemap == NULL)
+		return NULL;
+	else if (*sourcemap->w == 0 || *sourcemap->h == 0)
+		return NULL;
+		
+	map_index_last = (*sourcemap->w * *sourcemap->h) - 1;
+	
+	if (clip_origin_index > map_index_last)
+		return NULL;
+	
+	// this will always clip from center
+	// also make possible clip from topleft
+	delta_top = (int)floor(clip_origin_index / *sourcemap->w);
+	delta_bottom = *sourcemap->h - delta_top - 1;
+	delta_left = clip_origin_index - delta_top * *sourcemap->w;
+	delta_right = ((delta_top + 1) * *sourcemap->w) - clip_origin_index - 1;
+	
+	clip_y_top = clip_y_radius > delta_top ? delta_top : clip_y_radius;
+	clip_y_bottom = clip_y_radius > delta_bottom ? delta_bottom : clip_y_radius;
+	clip_x_left = clip_x_radius > delta_left ? delta_left : clip_x_radius;
+	clip_x_right = clip_x_radius > delta_right ? delta_right : clip_x_radius;
+	
+	clip_width = (clip_x_right + clip_x_left + 1);
+	clip_height = (clip_y_bottom + clip_y_top + 1);
+	
+	// we now have the size of the clip and the coords to cut
+	
+	lash_mapSimpleInit(clipmap, &clip_width, &clip_height);
+	
+	if (!include_path_only) {
+		for (i = 0; i < sourcemap->layer_count; i++) {
+			lash_mapSimpleAddLayer(clipmap);
+		}
+	}
+	
+	// and fill in with values from the main map
+	
+	for (i = clip_height - 1; i > -1; i--) {	
+		int cpos = clip_origin_index - (*sourcemap->w * (i - delta_bottom)) - clip_x_left;
+		memcpy((*clipmap->layers + ((clip_height - 1 - i) * clip_width)), (*sourcemap->layers + cpos), clip_width * sizeof(lash_map_simple_layer_item_t));
+	}	
+	
+	return clipmap;
 }
